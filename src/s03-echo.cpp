d717bae91ef5766d82960263d9b4cc35a9e7d03f
Mateusz Kamrowski <s23209@pjwstk.edu.pl>
2020-11-14 17:29:46 +0100

    Added src/s03-echo.cpp file

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <string>
 
-
-auto main(int argc, char* argv[]) -> int
-{
-    std::cout << "Lista argumentów: \n";
-    if (argc == 0) {
-        std::cout << "Brak argumentów do wyświetlenia";
-    }
-    auto i = 0;
-    while (i <= argc) {
-        std::cout << argv[i] << "\n";
-        i++;
-    }
-    return 0;
+auto main(int argc, char *argv[]) -> int {
+  std::cout << "Lista argumentów: \n";
+  if (argc == 0) {
+    std::cout << "Brak argumentów do wyświetlenia";
+  }
+  auto i = 0;
+  while (i <= argc) {
+    std::cout << argv[i] << "\n";
+    i++;
+  }
+  return 0;
 }
